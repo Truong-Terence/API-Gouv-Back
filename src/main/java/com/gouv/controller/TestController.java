d5384185d6b577d5445d1064282c71bbfc019383
Truong-Terence <tvn.terence@gmail.com>
2024-03-19 21:29:10 +0100

    make changes for DB

---
+++ src/main/java/com/gouv/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.gouv.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "test")
+public class TestController {
+
+    @GetMapping( path = "string")
+    public String getString() {
+        return "Hello from controller";
+    }
+
+}
