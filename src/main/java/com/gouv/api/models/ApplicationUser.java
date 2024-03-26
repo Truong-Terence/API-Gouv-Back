1bdceaf1b4e60faf7083345b6a19d0927fcd0b45
lucas.vittaz <lucasvittaz.pro@gmail.com>
2024-03-26 22:31:28 +0100

    feat: add authentification

--- src/main/java/com/gouv/api/models/ApplicationUser.java
+++ src/main/java/com/gouv/api/models/ApplicationUser.java
@@ -65,13 +65,11 @@ public class ApplicationUser implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
         return this.authorities;
     }
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
         return this.password;
     }
 
@@ -81,7 +79,6 @@ public class ApplicationUser implements UserDetails{
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
         return this.username;
     }
 
@@ -92,25 +89,21 @@ public class ApplicationUser implements UserDetails{
     /* If you want account locking capabilities create variables and ways to set them for the methods below */
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return true;
     }
 
